235c9f259fabc66941b4464273c3fbd1c021f862
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-29 14:49:28 -0800

    align

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,10 +10,11 @@ export default function Home() {
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
   const [font, setFont] = useState("Architect's Daughter")
+  const [align, setAlign] = useState('left')
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} />
+      <Preview title={title} subtitle={subtitle} font={font} align={align} />
       <Editor
         title={title}
         setTitle={setTitle}
@@ -21,6 +22,8 @@ export default function Home() {
         setSubtitle={setSubtitle}
         font={font}
         setFont={setFont}
+        align={align}
+        setAlign={setAlign}
       />
     </main>
   )
