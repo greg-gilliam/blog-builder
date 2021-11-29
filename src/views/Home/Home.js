07ce4dc3071c8c66bb8575092d1490dc5d868bb4
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-29 14:39:59 -0800

    title

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,12 +7,12 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-
+  const [title, setTitle] = useState('')
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} />
+      <Editor title={title} setTitle={setTitle} />
     </main>
   )
 }
